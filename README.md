bd22258a379439b842b4961ea43c8b1bafaed4fd
AdamantLife <adamantlife@gmail.com>
2025-03-10 21:44:11 -0400

    Intermediary Version

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # FlagCliAction
 
-While the `Flag` class from the `enum` module is very useful, it doesn't play well with `argparse` out of the box. This module shows how to implement a simple `Action` class to allow your `argparse` Command-line programs to utilize `Flags`.
+While the `Flag` class from the `enum` module is very useful, it doesn't play well with `argparse` out of the box.
+This module includes several utilities for working with `Flags` in `argparse`.
 
 ### Install
-The class is simple enough that it's more practical to copy/paste it into your code, but if you want to install it as a module you can run:
 ```
 pip install git+https://github.com/AdamantLife/FlagCliAction.git
-````
+```
 
 ### Usage
 Typical usage requires you to provide the following three arguments to `parser.add_argument()`:
@@ -32,3 +32,88 @@ parser.parse_args(['-a', '-b'])
 parser.parse_args(['-ba',])
 # > Namespace(flags=MyFlags.A|MyFlags.B)
 ```
+
+### Adding Multiple Flags at Once
+The module also provides `add_flag_group()` which can be used to more simply add `Flags`.
+```python
+class MyOtherFlags(Flag):
+    Delta = auto()
+    Echo = auto()
+    Foxtrot = auto()
+
+parser = ArgumentParser()
+add_flag_group(parser, MyOtherFlags)
+parser.print_help()
+# > usage: __init__.py [-h] [-d] [-e] [-f]
+# >
+# > options:
+# >   -h, --help     show this help message and exit
+# >
+# >   -d, --delta    Sets the flag MyOtherFlags.Delta
+# >   -e, --echo     Sets the flag MyOtherFlags.Echo
+# >   -f, --foxtrot  Sets the flag MyOtherFlags.Foxtrot
+
+parser.parse_args(["-de",])
+# > Namespace(myotherflags=<MyOtherFlags.Delta|Echo: 3>)
+```
+
+`add_flag_group()` can handle other input formats which can be used to customize the argument names (such as to avoid name collisions), use subsets of flags, or to include `Flags` from different Classes (though it is an error to assign `Flags` from different classes to the same `dest` since they cannot be `|` OR'd).
+
+### Adapting the Usage/Help Results
+The basic example has the following usage description:
+```
+usage: __init__.py [-h] [-a] [-b]
+
+options:
+  -h, --help  show this help message and exit
+  -a, --one   Sets the flag MyFlags.A        
+  -b          Sets the flag MyFlags.B        
+```
+This can be improved by using an argument group and supplying a name (and optionally description) or by using `add_flag_group()` and likewise assigning the `name` and `description` arguments.
+```python
+parser = ArgumentParser()
+flaggroup = parser.add_argument_group("MyFlags", "MyFlags for Doing Things")
+flaggroup.add_argument('-a', "--one", action=FlagAction, const=MyFlags.A, dest="flags")
+flaggroup.add_argument('-b', action=FlagAction, const=MyFlags.B, dest="flags")
+parser.print_help()
+# > usage: __init__.py [-h] [-a] [-b]
+# >
+# > options:
+# >   -h, --help  show this help message and exit
+# >
+# > MyFlags:
+# >   MyFlags for Doing Things
+# >
+# >   -a, --one   Sets the flag MyFlags.A
+# >   -b          Sets the flag MyFlags.B
+```
+
+### Alternative
+If you don't want to go through the trouble of adding an additional abstraction, one alternative is to use the `choices` and `nargs` argument and convert the result to `Flags` afterwards.
+
+```python
+parser = argparse.ArgumentParser()
+parser.add_argument("test")
+parser.add_argument("-f", "--flag", choices = ["a", "b", "c"], nargs="*",  default= [], help="MyFlags to set")
+
+args = parser.parse_args("test -f a b".split())
+
+flags = MyFlags(0)
+for flag in args.flag:
+    if flag == "a":
+        flags |= MyFlags.A
+    elif flag == "b":
+        flags |= MyFlags.B
+    elif flag == "c":
+        flags |= MyFlags.C
+    else:
+        ## This should never happen
+        raise ValueError(f"Unknown flag {flag}")
+print(flags)
+# > MyFlags.A|B
+```
+Note that all positional arguments (e.g.- *test*) ***must*** appear before any `nargs="*"|"+"` arguments. e.x.- `python mytest.py -f a b test` throws the error:
+
+`mytest.py: error: argument -f/--flag: invalid choice: 'test' (choose from 'a', 'b', 'c')` 
+
+You should probably include the `default=[]` argument so you don't need to test the `Namespace` value.
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,13 @@
-# Test Target
-from FlagCliAction import FlagAction
+# Test Targets
+from typing import NoReturn
+from FlagCliAction import FlagAction, add_flag_group
 # Test Framework
 import unittest
 
-from argparse import ArgumentParser
+from argparse import ArgumentParser, ArgumentError
 from enum import Flag, auto
 
-class TestCase(unittest.TestCase):
+class FlagActionTestCase(unittest.TestCase):
     class TestFlags(Flag):
         FLAG1 = auto()
         FLAG2 = auto()
@@ -62,6 +63,131 @@ class TestCase(unittest.TestCase):
         self.assertEqual(args.flags, self.TestFlags.FLAG1 | self.TestFlags.FLAG2)
         args = parser.parse_args(['-tr', '--one'])
         self.assertEqual(args.flags, self.TestFlags.FLAG1 | self.TestFlags.FLAG2 | self.TestFlags.FLAG3)
+
+class AddFlagGroupTestCase(unittest.TestCase):
+    class TestFlags(Flag):
+        FLAG1 = auto()
+        FLAG2 = auto()
+        FLAG3 = auto()
+    class OtherFlags(Flag):
+        FLAG4 = auto()
+        FLAG5 = auto()
+        FLAG6 = auto()
+    def test_basic(self):
+        parser = ArgumentParser()
+        add_flag_group(parser, self.TestFlags, dest='flags')
+        args = parser.parse_args(['--flag1', '--flag2'])
+        self.assertEqual(args.flags, self.TestFlags.FLAG1 | self.TestFlags.FLAG2)
+    def test_dest(self):
+        parser = ArgumentParser()
+        add_flag_group(parser, self.TestFlags, dest='myflags')
+        args = parser.parse_args(['--flag1', '--flag2'])
+        self.assertEqual(args.myflags, self.TestFlags.FLAG1 | self.TestFlags.FLAG2)
+    def test_nodest(self):
+        parser = ArgumentParser()
+        add_flag_group(parser, self.TestFlags)
+        args = parser.parse_args(['--flag1', '--flag2'])
+        self.assertEqual(args.testflags, self.TestFlags.FLAG1 | self.TestFlags.FLAG2)
+    def test_list_flags(self):
+        parser = ArgumentParser()
+        add_flag_group(parser, [self.TestFlags.FLAG1, self.TestFlags.FLAG3])
+        args = parser.parse_args(['--flag1', '--flag3'])
+        self.assertEqual(args.testflags, self.TestFlags.FLAG1 | self.TestFlags.FLAG3)
+    def test_list_name_flag_tuples(self):
+        parser = ArgumentParser()
+        add_flag_group(parser, [('flag1', self.TestFlags.FLAG1), ('flag3', self.TestFlags.FLAG3)])
+        args = parser.parse_args(['--flag1', '--flag3'])
+        self.assertEqual(args.testflags, self.TestFlags.FLAG1 | self.TestFlags.FLAG3)
+    def test_list_short_long_flag_tuples(self):
+        parser = ArgumentParser()
+        add_flag_group(parser, [('o', 'flag1', self.TestFlags.FLAG1), ('t', 'flag2', self.TestFlags.FLAG2)])
+        for test, result in [
+            (['-o', '--flag2'], self.TestFlags.FLAG1 | self.TestFlags.FLAG2),
+            (['-ot'], self.TestFlags.FLAG1 | self.TestFlags.FLAG2)]:
+            with self.subTest(test=test, result=result):
+                args = parser.parse_args(test)
+                self.assertEqual(args.testflags, result)
+    def test_overlapping_shortnames(self):
+        parser = ArgumentParser()
+        add_flag_group(parser, [('flag1', self.TestFlags.FLAG1), ('f', 'flag2', self.TestFlags.FLAG2)])
+        args = parser.parse_args(['--flag1', '-f'])
+        self.assertEqual(args.testflags, self.TestFlags.FLAG1 | self.TestFlags.FLAG2)
+        args = parser.parse_args(['-f', '--flag2'])
+        self.assertEqual(args.testflags, self.TestFlags.FLAG2)
+    def test_overlapping_shortnames_2(self):
+        parser = ArgumentParser()
+        add_flag_group(parser, [self.TestFlags.FLAG1, ('f', 'flag3', self.TestFlags.FLAG3)])
+        args = parser.parse_args(['--flag1', '-f'])
+        self.assertEqual(args.testflags, self.TestFlags.FLAG1 | self.TestFlags.FLAG3)
+        args = parser.parse_args(['-f', '--flag3'])
+        self.assertEqual(args.testflags, self.TestFlags.FLAG3)
+    def test_mixed_flag_classes_same_dest(self):
+        parser = ArgumentParser()
+        add_flag_group(parser, [self.TestFlags.FLAG1, self.OtherFlags.FLAG4], dest = "allflags")
+        for test, result in [
+            (['--flag1'], self.TestFlags.FLAG1),
+            (['--flag4'], self.OtherFlags.FLAG4)]:
+            with self.subTest(test=test, result=result):
+                args = parser.parse_args(test)
+                self.assertEqual(args.allflags, result)
+    def test_mixed_flag_classes_no_dest(self):
+        parser = ArgumentParser()
+        add_flag_group(parser, [self.TestFlags.FLAG1, self.OtherFlags.FLAG4])
+        for test, attr, result in [
+            (['--flag1'], "testflags", self.TestFlags.FLAG1),
+            (['--flag4'], "otherflags", self.OtherFlags.FLAG4)]:
+            with self.subTest(test=test, attr= attr, result=result):
+                args = parser.parse_args(test)
+                self.assertEqual(getattr(args, attr), result)
+
+
+    ## Bad Tests
+    def test_bad_longnames_flags(self):
+        parser = ArgumentParser()
+        with self.assertRaises(ValueError):
+            add_flag_group(parser, [self.TestFlags.FLAG1, self.TestFlags.FLAG1])
+    def test_bad_longnames_tuples(self):
+        parser = ArgumentParser()
+        with self.assertRaises(ValueError):
+            add_flag_group(parser, [('flag1', self.TestFlags.FLAG1), ('flag1', self.TestFlags.FLAG2)])
+    def test_bad_shortnames(self):
+        parser = ArgumentParser()
+        with self.assertRaises(ValueError):
+            add_flag_group(parser, [('o', 'flag1', self.TestFlags.FLAG1), ('o', 'flag2', self.TestFlags.FLAG2)])
+    def test_mixed_flag_classes_same_dest_bad(self):
+        parser = ArgumentParser()
+        add_flag_group(parser, [self.TestFlags.FLAG1, self.OtherFlags.FLAG4], dest = "allflags")
+        ## The TypeError is actually raised because "unsupported operand type(s) for |: 'TestFlags' and 'OtherFlags'"
+        ## Different Flag classes cannot have boolean operations performed on them
+        with self.assertRaises(TypeError):
+            parser.parse_args(['--flag1', '--flag4'])
+
+    ## Multiple Calls
+    def test_mulitple_calls(self):
+        parser = ArgumentParser()
+        add_flag_group(parser, [('o', 'flag1', self.TestFlags.FLAG1), ('t', 'flag2', self.TestFlags.FLAG2)])
+        add_flag_group(parser, [('r', 'flag3', self.TestFlags.FLAG3)])
+        args = parser.parse_args(['-otr'])
+        self.assertEqual(args.testflags, self.TestFlags.FLAG1 | self.TestFlags.FLAG2 | self.TestFlags.FLAG3)
+    def test_multiple_calls_2(self):
+        parser = ArgumentParser()
+        add_flag_group(parser, [('o', 'flag1', self.TestFlags.FLAG1), ('t', 'flag2', self.TestFlags.FLAG2)])
+        add_flag_group(parser, [self.TestFlags.FLAG3,])
+        args = parser.parse_args(['-o', '--flag3'])
+        self.assertEqual(args.testflags, self.TestFlags.FLAG1 | self.TestFlags.FLAG3)
+    def test_multiple_calls_bad_shortnames(self):
+        parser = ArgumentParser()
+        add_flag_group(parser, [('o', 'flag1', self.TestFlags.FLAG1), ('t', 'flag2', self.TestFlags.FLAG2)])
+        ## When multiple calls are made, the error is actually caught by the ArgumentParser
+        with self.assertRaises(ArgumentError):
+            add_flag_group(parser, [('o', 'flag3', self.TestFlags.FLAG3)])
+    def test_multiple_calls_bad_longnames(self):
+        parser = ArgumentParser()
+        add_flag_group(parser, [('o', 'flag1', self.TestFlags.FLAG1), ('t', 'flag2', self.TestFlags.FLAG2)])
+        ## When multiple calls are made, the error is actually caught by the ArgumentParser
+        with self.assertRaises(ArgumentError):
+            add_flag_group(parser, [('r', 'flag1', self.TestFlags.FLAG3)])
+
 
 if __name__ == '__main__':
     unittest.main()
